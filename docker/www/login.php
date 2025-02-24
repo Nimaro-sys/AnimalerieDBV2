e0de957e9cb952f6330569fbddcfe6f1cf01653a
Nimaro-sys <romain.hermenegilde.dwwm48@gmail.com>
2025-02-24 09:58:13 +0100

    Update login.php

--- docker/www/login.php
+++ docker/www/login.php
@@ -21,16 +21,10 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
         $error = "Nom d'utilisateur ou mot de passe incorrect.";
     }
 }
+
+include_once 'include/header.php';
 ?>
 
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion Employé</title>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css">
-</head>
 <body>
 
 <div class="container mt-5">
@@ -53,5 +47,6 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
 
-</body>
-</html>
+<?php 
+include_once 'include/footer.php'; 
+?>
